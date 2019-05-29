d762f5fd90c360fe695d9f9e57103d75d0c52e8a
Igor Kwiatkowski <46054245+IgorKwiatkowski@users.noreply.github.com>
2019-05-29 19:57:09 +0200

    Add files via upload

---
+++ 99 bottles of beer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+bottles = 99
+
+while bottles >1:
+    print(f"{bottles} bottles of beer on the wall,\n{bottles} bottles of beer, take one down pass it around.\n")
+    bottles -=1
+
+print(f"{bottles} bottle of beer on the wall,\n{bottles} bottle of beer, take one down pass it around.")
+
+
+# In[ ]:
+
+
+
+
