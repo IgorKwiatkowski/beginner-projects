66288ad2b40fda4334ec0e85afa4559f7af0078b
Igor Kwiatkowski <46054245+IgorKwiatkowski@users.noreply.github.com>
2019-06-01 16:32:30 +0200

    Add files via upload

---
+++ Factors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def factors(n):
+    fac_list = []
+    for num in range (1,n+1):
+        if n % num == 0:
+            fac_list.append(num)
+    print(fac_list)
+
+
+# In[6]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
